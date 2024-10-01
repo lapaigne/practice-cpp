1d379a67df1b06f478f310f84ae8cf248f08c720
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2024-10-01 17:17:41 +0400

    lab 3 done

--- third.cpp
+++ third.cpp
@@ -27,7 +27,7 @@ void task3_2()
 {
 	int n;
 	int factorial = 1;
-	int power = 1;
+	double power = 1;
 	double sum = 0;
 	cout << "N = ";
 	cin >> n;
@@ -37,6 +37,25 @@ void task3_2()
 		factorial *= i;
 		power *= 3;
 		sum += power / factorial;
+	}
+
+	cout << "Сумма: " << sum << endl;
+}
+
+void task3_3()
+{
+	int n;
+	double power = 1;
+	double sum = 0;
+	double subsum = 0;
+	cout << "N = ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		subsum += 1.0 / i;
+		power *= 2;
+		sum += power / subsum;
 	}
 
 	cout << "Сумма: " << sum << endl;
@@ -63,7 +82,7 @@ void task3_5()
 {
 	int n;
 	int factorial = 1;
-	double prod = 0;
+	double prod = 1;
 	cout << "N = ";
 	cin >> n;
 
@@ -76,16 +95,34 @@ void task3_5()
 	cout << "Произведение: " << prod << endl;
 }
 
+void task3_6()
+{
+	int n;
+	int factorial = 1;
+	double prod = 1;
+	double power = 1;
+	cout << "N = ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		power *= 2;
+		factorial *= i;
+		prod *= (power + 0.5 * (i + 1) * i / factorial);
+	}
+
+	cout << "Произведение: " << prod << endl;
+}
+
 void task3_7()
 {
 	int n;
-	int result = 0;
-	double prod = 0;
+	double result = 0;
 	cout << "N = ";
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		result += sqrt(result + 1);
+		result = sqrt(result + 1);
 	}
 
 	cout << "Результат: " << result << endl;
@@ -94,13 +131,12 @@ void task3_7()
 void task3_9()
 {
 	int n;
-	int result = 0;
-	double prod = 0;
+	double result = 0;
 	cout << "N = ";
 	cin >> n;
 	for (int i = 1; i <= n; i++)
 	{
-		result += sqrt(result + i);
+		result = sqrt(result + i);
 	}
 
 	cout << "Результат: " << result << endl;
@@ -109,16 +145,54 @@ void task3_9()
 void task3_10()
 {
 	int n;
-	int result = 0;
-	double prod = 0;
+	double result = 0;
 	cout << "N = ";
 	cin >> n;
 	for (int i = 1; i <= n; i++)
 	{
-		result += sqrt(result + 3 * i);
+		result = sqrt(result + 3 * i);
 	}
+	
 
 	cout << "Результат: " << result << endl;
+}
+
+void task3_12()
+{
+	int n;
+	double x;
+
+	cout << "n = ";
+	cin >> n;
+	cout << "x = ";
+	cin >> x;
+	
+	double power = x;
+
+	int imax = 1; 
+	int imin = 1;
+	
+	double max = sin(power);
+	double min = sin(power);
+
+	for (int i = 2; i <= n; i++)
+	{
+		power *= x;
+		double current = i * sin(power);
+		if (current > max)
+		{
+			imax = i;
+			max = current;
+		}
+		if (current < min)
+		{
+			imin = i;
+			min = current;
+		}
+	}
+
+	cout << "Max: " << max << "\t (" << imax << ")" << endl;
+	cout << "Min: " << max << "\t (" << imin << ")" << endl;
 }
 
 void task3_14()
@@ -152,7 +226,7 @@ void task3_14()
 void task3_16()
 {
 	int m, n;
-	int count;
+	int count = 0;
 
 	cout << "M = ";
 	cin >> m;
@@ -177,18 +251,24 @@ void task3_16()
 		{
 			count++;
 		}
-		
-		cout << "Результат: " << count << endl;
 	}
+
+	cout << "Результат: " << count << endl;
 }
 
 void task3_17()
 {
-	int r;
+	double r;
 	int count = 0;
 
 	cout << "R = ";
 	cin >> r;
+
+	if (r < 0)
+	{
+		cout << "Неправльные данные!" << endl;
+		return;
+	}
 
 	for (int i = -r; i <= r; i++)
 	{
@@ -207,19 +287,25 @@ void task3_17()
 
 void task3_18()
 {
-	int r1, r2;
+	double r1, r2;
 	int count = 0;
 	
-	cout << "(r1, r2): ";
-	cin >> r1 >> r2;
+	cout << "R1 = ";
+	cin >> r1;
+	cout << "R2 = ";
+	cin >> r2;
 
-	int rsq = r1 * r1 + r2 * r2;
-
-	for (int i = - floor(r1); i <= r1; i++)
+	if (r1 < 0 || r2 < 0)
 	{
-		for (int j = -floor(r2); j <= r2; j++)
+		cout << "Неправльные данные!" << endl;
+		return;
+	}
+
+	for (int i = -max(r1,r2); i <= max(r1, r2); i++)
+	{
+		for (int j = -max(r1, r2); j <= max(r1, r2); j++)
 		{
-			if (i * i + j * j <= rsq)
+			if (i * i + j * j <= max(r1, r2) * max(r1, r2) && i * i + j * j >= min(r1, r2) * min(r1, r2))
 			{
 				count++;
 			}
@@ -229,23 +315,61 @@ void task3_18()
 	cout << "Кол-во точек, попавших в окружность: " << count << endl;
 }
 
+void task3_20()
+{
+	int m, n;
+	
+	cout << "N = ";
+	cin >> n;
+	cout << "M = ";
+	cin >> m;
+
+	if (m < n || m > 1000 || n > 1000)
+	{
+		cout << "Неправльные данные!" << endl;
+		return;
+	}
+
+	for (int i = n; i <= m; i++)
+	{
+		int count = 0;
+		for (int j = 1; j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				count++;
+			}
+		}
+
+		if (count == 2)
+		{
+			cout << i << endl;
+		}
+
+	}
+}
+
 void third()
 {
-	task3_1();
-	task3_2();
-	task3_4();
-	task3_5();
-	task3_7();
-
-	// ---
-	
-	task3_9();
-	task3_10();
-	task3_14();
-	task3_16();
-	task3_17();
+	//task3_1();
+	//task3_2();
+	//task3_3();
+	//task3_4();
+	//task3_5();
 
 	// ---
 
-	task3_18();
+	//task3_6();
+	//task3_7();
+	//task3_9();
+	//task3_10();
+	//task3_12();
+
+	// ---
+
+	//task3_14();
+	//task3_16();
+	//task3_17();
+	//task3_18();
+	//task3_20();
 }
